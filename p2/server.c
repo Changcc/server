2a22835c31ae82de2b841b74481b6f411fa99437
changcc5 <changcc5@cs.ucla.edu>
2014-12-04 17:26:59 -0800

    bad address?

--- p2/server.c
+++ p2/server.c
@@ -4,11 +4,11 @@ UPD GBN N=4
 #include<stdio.h> //printf
 #include<string.h> //memset
 #include<stdlib.h> //exit(0);
+#include <unistd.h> // for close
 #include<arpa/inet.h>
 #include<sys/socket.h>
 #include "packet.c"
  
-#define BUFLEN 1000  //Max length of buffer
 #define N 4 //window size
  
 void die(char *s)
@@ -16,20 +16,30 @@ void die(char *s)
     perror(s);
     exit(1);
 }
+double chance()
+{
+    return (double)rand() / (double)RAND_MAX;
+}
  
 int main(int argc, char *argv[])
 {
+	double p_loss, p_corrupt;
+    p_loss = 0.0;
+    p_corrupt = 0.0;
+    
     struct sockaddr_in serv_si, cli_si;
+	struct packet *rcvpkt;
+    FILE *file;
      
-    int portno, sockfd, i, slen = sizeof(cli_si) , recv_len;
-    char buf[BUFLEN];
+    int portno, sockfd, i, recv_len;
+    int slen = sizeof(cli_si);
      
     portno = atoi(argv[1]);
     
     //create a UDP socket
-    if ((sockfd=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    if ((sockfd=socket(AF_INET, SOCK_DGRAM, 0)) == -1)
     {
-            exit(1);
+            die("socketfd error");
     }
      
     // zero out the structure
@@ -40,95 +50,95 @@ int main(int argc, char *argv[])
     serv_si.sin_addr.s_addr = htonl(INADDR_ANY);
      
     //bind socket to port
-    
-    struct packet rcvpkt;
     if( bind(sockfd , (struct sockaddr*)&serv_si, sizeof(serv_si) ) == -1)
     {
-            exit(1);
+            die("error in bind\n");
     }
-    if ((recv_len = recvfrom(sockfd, &pkt, BUFLEN, 0, (struct sockaddr *) &cli_si, &slen)) == -1)
+    
+    printf("Waiting for syn...\n");
+    if ((recv_len = recvfrom(sockfd, rcvpkt, sizeof(rcvpkt), 0, (struct sockaddr *) &cli_si, (socklen_t*)&slen)) == -1)
 	{
-        exit(1);
+        die("error receive syn");
 	}
+
+    printf("%d\n", recv_len);
+    printf("%s\n", rcvpkt->data);
+    printf("%d\n", check_syn(rcvpkt));
     
-    if (checksyn(&rcvpkt) != 0) {
+    if (check_syn(rcvpkt) != 0) {
     	//received syn flag, start connection, send packets
-    	FILE *file;
-    	file = fopen(rcvpkt.data, "r");
-		fread(buf, sizeof(char),  sizeof(buf), file);
+    	file = fopen(rcvpkt->data, "r");
     }
     
-    int base = 1;
+    int base = 0;
     int ack; //last seq ack'd
-    int nextseq = 1; //next seq count
-    int pid; //child pid
+    int nextseq = 0; //next seq count
+    int timer, rcvr; //timer, rcvr pid
     struct packet pkt[N];
+    char refuse_data = 'f'; //f for false t for true 
+    char timeout = 'f';
     
-    pid = fork();
-    
-    if (pid == 0) {
+    timer = fork();
+    if (timer == 0) { //in charge of timer
     	//signals timeout
     	//receives signal to start timer
     	
     }
     
-    while (nextseq < base + N) {
-    	//while within window, keep sending packet
-    	
-    	if (nextseq == base) {
-    		//start timer
+    rcvr = fork();
+    if (rcvr == 0) { //in charge of receiving ack
+    	while (refuse_data == 'f') {
+    		struct packet *rcvpkt;
+    		if ((recv_len = recvfrom(sockfd, rcvpkt, sizeof(rcvpkt), 0, (struct sockaddr *) &cli_si, (socklen_t*)&slen)) == -1)
+			{
+		        printf("Packet from sender LOST\n");
+			}
+			else if (chance() < p_corrupt)
+			{
+				printf("Packet from sender CORRUPT\n");
+			}
+			else {
+				base = rcvpkt->ack_num + DATA_SIZE;
+			}
     	}
-    	nextseq++;
     }
-    /*
-    	when receive ack, update ack int
-    */
-    //keep listening for data
-   //  while(1)
-//     {
-//         printf("Waiting for data...");
-//         fflush(stdout);
-//         struct packet pkt;
-//          
-//         //try to receive some data, this is a blocking call
-//         if ((recv_len = recvfrom(sockfd, &pkt, BUFLEN, 0, (struct sockaddr *) &cli_si, &slen)) == -1)
-//         {
-//             //exit(1);
-//         }
-//         
-//         //print details of the client/peer and the data received
-//         printf("Received packet from %s:%d\n", inet_ntoa(cli_si.sin_addr), ntohs(cli_si.sin_port));
-//         
-// //         printf("Seq Num: %d\n", pkt.seq_num);
-//         printf("Ack Num: %d\n", pkt.ack_num);
-//         if (check_syn(&pkt) != 0) {
-//         	printf("Flag: Syn\n");
-//         	struct packet spkt;
-//         	
-//         	spkt.seq_num = 1;
-//         	spkt.ack_num = pkt.ack_num + 1;
-//         	
-//         	spkt.flags = SYN_FLAG | ACK_FLAG;
-//         	
-//         }
-//         else if (check_ack(&pkt) != 0) {
-//         	printf("Flag: Ack\n");
-//         }
-//         else if (check_fin(&pkt)!= 0){
-//         	printf("Flag: Fin\n");
-//         }
-//         else {
-//         	printf("Flag: None\n");
-//         }
-//         
-//         //now reply the client with the same data
-//         if (sendto(sockfd, buf, recv_len, 0, (struct sockaddr*) &cli_si, slen) == -1)
-//         {
-//             //exit(1);
-//         }
-//         
-//     }
- 
+    
+    while (1) { //in charge of sending packets
+    	if (timeout == 't') {
+    		int n_char;
+    		int resend_base = base;
+    		while (resend_base < nextseq) {
+				n_char = sendto(sockfd, &pkt[resend_base], sizeof(&pkt[resend_base]), 0, (struct sockaddr*)&cli_si, slen);
+				if (n_char < 0)
+				{
+					die("Error sending packet");
+				}
+				resend_base++;
+			}
+    	}
+    	
+		if (nextseq < base + N) {
+			//while within window, keep sending packet
+			char buf[DATA_SIZE];
+			fread(buf, sizeof(char),  sizeof(buf), file);
+			struct packet *nextpkt = make_packet();
+			set_data(nextpkt, buf);
+			
+    		int n_char;
+			n_char = sendto(sockfd, &pkt[nextseq], sizeof(&pkt[nextseq]), 0, (struct sockaddr*)&cli_si, slen);
+			if (n_char < 0) {
+				die("Error sending packet");
+			}
+			if (nextseq == base) {
+				//start timer
+			}
+			nextseq++;
+		}
+		else {
+			refuse_data = 't';
+		}
+    }
+    
     close(sockfd);
     return 0;
 }
